a6455a7680ef3787b9eefda4bc20a6908892002f
julia-anjos968 <julia.r.anjos@aluno.senai.br>
2025-08-25 11:03:25 -0300

    feat: cria o arquivo dados.js e exporta no server.js

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 import express from "express";
-import bruxos from "./src/data/bruxos.js";
+import dados from "./src/data/dados.js";
+
+const {bruxos} = dados;
+const {casas} = dados;
+const {varinhas} = dados;
+const {animais} = dados;
+const {pocoes} = dados;
 
 const serverPort = 3000;
 
@@ -13,10 +19,104 @@ app.get ("/", (req, res) => {
 
 // Rota dos bruxos
 app.get("/bruxos", (req, res) => {
-    res.json(bruxos);
+    if (bruxos.length >0 ) { 
+        res.status(200).json(bruxos);
+} else {
+    res.status(404).json ({
+        mensagem: "Nenhum bruxo encontrado!"
+    })
+}
+})
+
+// Rota das casas
+app.get("/casas", (req,res) => {
+    if (casas.length >0) {
+        res.status(200).json(casas);
+} else {
+    res.status(404).json ({
+        mensagem: "Nenhuma casa encontrada!"
+    })
+}
+})
+
+// Rota das varinhas
+app.get("/varinhas", (req, res) => {
+    if (varinhas.length > 0) {
+        res.status(200).json(varinhas);
+} else {
+    res.status(404).json ({
+        mensagem: "Nenhuma varinha encontrada!"
+    })
+    }
 });
 
+//Rota dos animais
+app.get("/animais", (req, res) => {
+    if (animais.length > 0) {
+        res.status(200).json(animais);
+} else {
+    res.status(404).json ({
+        mensagem: "Nenhum animal encontrado!"
+    })
+    }
+});
+
+// Rota das poções
+app.get("/pocoes", (req, res) => {
+  if (pocoes.length > 0) {
+        res.status(200).json(pocoes);
+} else {
+    res.status(404).json ({
+        mensagem: "Nenhuma poção encontrada!"
+    })
+    }
+});
+
+// Rota do GetById varinhas
+app.get ("/varinhas/:id", (req, res) => {
+    const id = parseInt (req.params.id);
+
+    const varinha = varinhas.find(v => v.id === id);
+
+    if (varinha) {
+  res.status(200).json(varinha);
+} else {
+  res.status(404).json({
+    mensagem: "varinha nao encontrada!",
+  })
+}
+});
+
+// Rota do GetById animais
+app.get ("/animais/:id", (req, res) => {
+    const id = parseInt (req.params.id);
+
+    const animal = animais.find(a => a.id === id);
+
+    if (animal) {
+  res.status(200).json(animal);
+} else {
+  res.status(404).json({
+    mensagem: "animal nao encontrado!",
+  })
+}
+})
+
+// Rota do GetById poções
+app.get ("/pocoes/:id", (req, res) => {
+    const id = parseInt (req.params.id);
+
+    const pocao = pocoes.find(p => p.id === id);
+
+    if (pocao) {
+  res.status(200).json(pocao);
+} else {
+  res.status(404).json({
+    mensagem: "Pocao nao encontrada!",
+  })
+}
+})
 
 app.listen(serverPort,() => {
-    console.log(`Api dos bruxos está no ar http://localhost:${serverPort}`);
+    console.log(`Api dos bruxos está no ar http://localhost:${serverPort} !!!`);
 })
---
+++ src/data/dados.js
@@ -0,0 +1,130 @@
+const bruxos = [
+    {
+        id: 1,
+        nome: "Harry Potter",
+        ano: 7,
+        varinha: "Azevinho e pena de fênix, 28cm",
+        mascote: "Coruja (Edwiges)",
+        patrono: "Cervo",
+        especialidade: "Sobreviver a Voldemort 😎",
+        vivo: false
+    },
+    {
+        id: 2,
+        nome: "Hermione Granger",
+        casa: "Grifinória",
+        ano: 7,
+        varinha: "Videira e fibra de coração de dragão, 27cm",
+        mascote: "Gato (Bichento)",
+        patrono: "Lontra",
+        especialidade: "Saber tudo de tudo",
+        vivo: true
+    }
+];
+
+const casas = [
+    {
+        id: 1,
+        nome: "Grifinória",
+        fundador: "Godric Gryffindor",
+        cores: "Vermelho e Dourado",
+        animal: "Leao"
+    },
+    {
+        id: 2,
+        nome: "Sonserina",
+        fundadora: "Salazar Slytherin",
+        cores: "Verde e prata",
+        animal: "Serpente"
+    },
+    {
+        id: 3,
+        nome: "Corvinal",
+        fundadora: "Rowena Ravenclaw",
+        cores: "Azul e Bronze",
+        animal: "Águia"
+    },
+    {
+        id: 4,
+        nome: "Lufa-Lufa",
+        fundadora: "Helga Hufflepuff",
+        cores: "Amarelo e Preto",
+        animal: "Texugo"
+    },
+]
+
+const varinhas = [
+    {
+        id: 1,
+        material: "Azevinho",
+        nucleo: "Pena de Fênix",
+        comprimento: "28cm"
+    },
+    {
+        id: 2,
+        material: "Videira",
+        nucleo: "Fibra de Coração de Dragão",
+        comprimento: "27cm"
+    },
+    {
+        id: 3,
+        material: "Salgueiro",
+        nucleo: "Pelo de Unicórnio",
+        comprimento: "35cm"
+    },
+    {
+        id: 4,
+        material: "Carvalho Inglês",
+        nucleo: "Pelo de Testrálio",
+        comprimento: "32cm"
+    }
+];
+
+const animais = [
+    {
+        id: 1,
+        nome: "Coruja",
+        tipo: "Correio/Companhia"
+    },
+    {
+        id: 2,
+        nome: "Gato",
+        tipo: "Companhia"
+    },
+    {
+        id: 3,
+        nome: "Sapo",
+        tipo: "Companhia"
+    },
+    {
+        id: 4,
+        nome: "Rato",
+        tipo: "Companhia"
+    }
+];
+
+const pocoes = [
+    {
+        id: 1,
+        nome: "Polissuco",
+        efeito: "Transforma na aparência de outra pessoa"
+    },
+    {
+        id: 2,
+        nome: "Felix Felicis",
+        efeito: "Sorte temporária"
+    },
+    {
+        id: 3,
+        nome: "Amortentia",
+        efeito: "Poção do amor"
+    },
+    {
+        id: 4,
+        nome: "Veritaserum",
+        efeito: "Força a dizer a verdade"
+    }
+];
+
+
+export default {bruxos, casas, varinhas, animais, pocoes};
